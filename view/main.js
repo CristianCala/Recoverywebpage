5fa435cfd9a24cbbae9e58e7ff2a82ae06626c28
Cristian Cala <cristiancala@protonmail.com>
2020-12-20 13:10:52 -0400

    Más animaciones, y mejoras footer

--- view/main.js
+++ view/main.js
@@ -4,20 +4,43 @@ window.onload = function() {
 
 	let animacion2 = document.getElementById('animatedstatic2')
 
-	animacion.style.animation = 'mover 1s ease-out';
-	animacion2.style.animation = 'mover2 1s ease-out';
+	animacion.style.animation = 'moverderecho 1s ease-out';
+	animacion2.style.animation = 'moverarriba 1s ease-out';
 }
+
+
+// Funcion para animaciones generales 
+window.addEventListener('scroll', function(){
+
+	let animacionderecha = document.getElementById('animacionderecha')
+	let	animacionizquierda = document.getElementById('animacionizquierda')
+
+	let posicionObject = animacionderecha.getBoundingClientRect().top;
+	let posicionObject2 = animacionizquierda.getBoundingClientRect().top;
+
+
+	let tamañoPantalla = window.innerHeight;
+
+	if (posicionObject < tamañoPantalla){
+		animacionderecha.style.animation = 'moverderecho 1s ease-out'
+	}
+
+	if (posicionObject2 < tamañoPantalla){
+		animacionizquierda.style.animation = 'moverizquierda 1s ease-out'
+	}
+
+})
 
 // Funcion para opacidad del footer (logo)
 window.addEventListener('scroll', function(){
 
 	let animacion = document.getElementById('logoRecoveryloid')
 	let posicionObject = animacion.getBoundingClientRect().top;
-
 	let tamañoPantalla = window.innerHeight;
 
 	if (posicionObject < tamañoPantalla){
 		animacion.style.animation = 'logo 5s ease-out'
+
 	}
 
 })

d152592efc26f3227316a3484a5d6b629c7b2197
CristianCala <cristiancala@protonmail.com>
2021-01-03 19:31:44 -0430

    Nuevo section y animaciones

--- view/main.js
+++ view/main.js
@@ -14,19 +14,25 @@ window.addEventListener('scroll', function(){
 
 	let animacionderecha = document.getElementById('animacionderecha')
 	let	animacionizquierda = document.getElementById('animacionizquierda')
+	let animacionarriba = document.getElementById('animacionarriba')
 
 	let posicionObject = animacionderecha.getBoundingClientRect().top;
 	let posicionObject2 = animacionizquierda.getBoundingClientRect().top;
+	let posicionObject3 = animacionarriba.getBoundingClientRect().top;
 
 
 	let tamañoPantalla = window.innerHeight;
 
 	if (posicionObject < tamañoPantalla){
-		animacionderecha.style.animation = 'moverderecho 1s ease-out'
+		animacionderecha.style.animation = 'moverderecho 1s ease-out';
 	}
 
 	if (posicionObject2 < tamañoPantalla){
-		animacionizquierda.style.animation = 'moverizquierda 1s ease-out'
+		animacionizquierda.style.animation = 'moverizquierda 1s ease-out';
+	}
+
+	if (posicionObject3 < tamañoPantalla){
+		animacionarriba.style.animation = 'moverarriba 1s ease-out';
 	}
 
 })
@@ -39,8 +45,22 @@ window.addEventListener('scroll', function(){
 	let tamañoPantalla = window.innerHeight;
 
 	if (posicionObject < tamañoPantalla){
-		animacion.style.animation = 'logo 5s ease-out'
+		animacion.style.animation = 'logo 5s ease-out';
 
 	}
 
+})
+
+
+window.addEventListener('scroll', function(){
+
+	let animaciondere = document.getElementById('contenedordere')
+
+	let posicionObject = animaciondere.getBoundingClientRect().top;
+
+	let tamañoPantalla = window.innerHeight;
+
+	if (posicionObject < tamañoPantalla){
+		animaciondere.style.animation = 'moverderecho 1s ease-out';
+	}
 })
